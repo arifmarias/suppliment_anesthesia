f4b882bdde74e1d760f1e3b42802f3290907c9dc
arifmarias <arif.marias@gmail.com>
2023-06-21 14:10:28 +0800

    add db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import calendar
 import pandas as pd
 from pycaret.classification import *
 # import database as db
-
+import database as db  # local import
 # -------------- SETTINGS --------------
 page_title = "Suppliment Anesthesia Prediction"
 page_icon = "🦷"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -41,32 +41,32 @@ tabs_font_css = """
 
 div[class*="stMultiSelect"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 
 div[class*="stSlider"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 
 div[class*="stRadio"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 
 div[class*="stSelectbox"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 
 div[class*="stTextInput"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 
 div[class*="stNumberInput"] label p {
   font-weight:bold;
-  color: green;
+  color: white;
 }
 </style>
 """
@@ -80,64 +80,99 @@ d = st.date_input("Date")
 year = d.year
 month = d.month
 day = d.day
-# --- Form Input ---
-with st.form("entry_form", clear_on_submit=True):
-        with st.expander("Patient Information"):
-              patient_name = st.text_input("Name of the Patient")
-              reg_no = st.text_input("Registration No.")
-              Patient_Gender = st.radio("Gender", options=("Male","Female"), horizontal=True)
-              Patient_Age = st.slider('Age', 0, 100,step=1)
-        with st.expander("Dental Related Info"):
-            Dental_History =st.radio("Previous Dental Treatment", options=("Yes","No"), horizontal=True)
-            Pulp_Stone_or_Calcification = st.radio("Pulp Stone or Calcification", options=("Yes","No"), horizontal=True)
-            Curved_Canal =st.radio("Curved Canal", options=("Yes","No"), horizontal=True)
-            Mobility = st.radio("Mobility", options=("Yes","No"), horizontal=True)
-            Medical_History = st.radio("Medical History", options=["Yes","No"], index =1, horizontal=True)
-            periodontal_space = st.radio('Periodontal Space', options=("Yes","No"), horizontal=True)
-            PDL_Ligament_involvement = st.radio("Periodontal Ligament Involvement", options=("Yes","No"), horizontal=True)
-            Palpation = st.radio("Palpation", options=("Positive","Negative"), horizontal=True)
-            Lamina_Dura = st.radio("Lamina Dura", options=("Loss","Intact"), horizontal=True)
-            Percussion_Test = st.radio("Percission Test",("Positive", "Negative"), horizontal=True)
-            Pain_Duration_Days = st.number_input("Pain Duration (in Days)",format='%0.0f')
-            EPT_duration_before_anaesthesia_seconds = st.number_input("Electrical Pulp Test Response's Duration (in seconds)",format='%0.0f')
-            EPT_current_pass = st.number_input("Electric Pulp Test's Current Pass (in Unit)",format='%0.0f')
-            Cold_Test_Pain_Duration_before_anaesthesia_seconds = st.number_input("Cold Test Pain Duration (in seconds)",format='%0.0f')
-            st.image("http://www.wikidoc.org/images/e/eb/Pain_scale.jpg",width=250)
-            Cold_test_VAS_Score_Before_anaesthesia = st.slider('Cold Test (VAS) score', 0, 10,step=1)
-            Clinical_Pain_VAS_Score = st.slider('Clinical Pain (VAS) score', 0, 10,step=1)
-            EPT_VAS_before_anaesthesia = st.slider('Electric Pulp Test (VAS) response score', 0, 10,step=1)
-            
-            local_Anesthetic = st.selectbox("Type of Anaesthesia Provided",("ASANB","IANB+IN","IANB+LB","INCISIVE NB",
-                                                                                 "Mental NB","MSAN","MSAN+ PSAN","PSAN","PSAN+MSAN"))
-            comment = st.text_area("Any Other Comment", placeholder="Enter a comment here ...")
-            if (local_Anesthetic == "IANB+IN") or (local_Anesthetic == "INCISIVE NB") or (local_Anesthetic == "Mental NB") or (local_Anesthetic == "IANB+LB"):
-                  local_Anesthetic = "Mandible"
-            else:
-                  local_Anesthetic = "Maxilla"
-        
-        "---"
-        data = [[Patient_Gender,Patient_Age,Pulp_Stone_or_Calcification,
-                 Pain_Duration_Days,Percussion_Test,Palpation,EPT_duration_before_anaesthesia_seconds,
-                 Dental_History,Curved_Canal,Cold_Test_Pain_Duration_before_anaesthesia_seconds,
-                 Cold_test_VAS_Score_Before_anaesthesia,Clinical_Pain_VAS_Score,Mobility,Medical_History,
-                 EPT_current_pass,EPT_VAS_before_anaesthesia,periodontal_space,PDL_Ligament_involvement,
-                 Lamina_Dura,local_Anesthetic]]          
-        df = pd.DataFrame(data,columns=['Patient_Gender','Patient_Age','Pulp_Stone_or_Calcification','Pain_Duration_Days'
-                                        ,'Percussion_Test','Palpation','EPT_duration_before_anaesthesia_seconds'
-                                        ,'Dental_History','Curved_Canal','Cold_Test_Pain_Duration_before_anaesthesia_seconds'
-                                        ,'Cold_test_VAS_Score_Before_anaesthesia','Clinical_Pain_VAS_Score','Mobility'
-                                        ,'Medical_History','EPT_current_pass','EPT_VAS_before_anaesthesia','PDL_Space'
-                                        ,'PDL_Ligament_involvement','Lamina_Dura','local_Anesthetic'])
-        submitted = st.form_submit_button("Submit Data for Prediction")
 
-if submitted:
+with st.container():
+    # --- Form Input ---
+  with st.form("entry_form", clear_on_submit=True):
+          with st.expander("Patient Information"):
+                patient_name = st.text_input("Name of the Patient")
+                reg_no = st.text_input("Registration No.")
+                Patient_Gender = st.radio("Gender", options=("Male","Female"), horizontal=True)
+                Patient_Age = st.slider('Age', 0, 100,step=1)
+          with st.expander("Dental Related Info"):
+              Dental_History =st.radio("Previous Dental Treatment", options=("Yes","No"), horizontal=True)
+              Pulp_Stone_or_Calcification = st.radio("Pulp Stone or Calcification", options=("Yes","No"), horizontal=True)
+              Curved_Canal =st.radio("Curved Canal", options=("Yes","No"), horizontal=True)
+              Mobility = st.radio("Mobility", options=("Yes","No"), horizontal=True)
+              Medical_History = st.radio("Medical History", options=["Yes","No"], index =1, horizontal=True)
+              periodontal_space = st.radio('Periodontal Space', options=("Yes","No"), horizontal=True)
+              PDL_Ligament_involvement = st.radio("Periodontal Ligament Involvement", options=("Yes","No"), horizontal=True)
+              Palpation = st.radio("Palpation", options=("Positive","Negative"), horizontal=True)
+              Lamina_Dura = st.radio("Lamina Dura", options=("Loss","Intact"), horizontal=True)
+              Percussion_Test = st.radio("Percission Test",("Positive", "Negative"), horizontal=True)
+              Pain_Duration_Days = st.number_input("Pain Duration (in Days)",format='%0.0f')
+              EPT_duration_before_anaesthesia_seconds = st.number_input("Electrical Pulp Test Response's Duration (in seconds)",format='%0.0f')
+              EPT_current_pass = st.number_input("Electric Pulp Test's Current Pass (in Unit)",format='%0.0f')
+              Cold_Test_Pain_Duration_before_anaesthesia_seconds = st.number_input("Cold Test Pain Duration (in seconds)",format='%0.0f')
+              st.image("http://www.wikidoc.org/images/e/eb/Pain_scale.jpg",width=250)
+              Cold_test_VAS_Score_Before_anaesthesia = st.slider('Cold Test (VAS) score', 0, 10,step=1)
+              Clinical_Pain_VAS_Score = st.slider('Clinical Pain (VAS) score', 0, 10,step=1)
+              EPT_VAS_before_anaesthesia = st.slider('Electric Pulp Test (VAS) response score', 0, 10,step=1)
+              
+              local_Anesthetic = st.selectbox("Type of Anaesthesia Provided",("ASANB","IANB+IN","IANB+LB","INCISIVE NB",
+                                                                                  "Mental NB","MSAN","MSAN+ PSAN","PSAN","PSAN+MSAN"))
+              comment = st.text_area("Any Other Comment", placeholder="Enter a comment here ...")
+              if (local_Anesthetic == "IANB+IN") or (local_Anesthetic == "INCISIVE NB") or (local_Anesthetic == "Mental NB") or (local_Anesthetic == "IANB+LB"):
+                    local_Anesthetic = "Mandible"
+              else:
+                    local_Anesthetic = "Maxilla"
+          
+          "---"
+          data = [[Patient_Gender,Patient_Age,Pulp_Stone_or_Calcification,
+                  Pain_Duration_Days,Percussion_Test,Palpation,EPT_duration_before_anaesthesia_seconds,
+                  Dental_History,Curved_Canal,Cold_Test_Pain_Duration_before_anaesthesia_seconds,
+                  Cold_test_VAS_Score_Before_anaesthesia,Clinical_Pain_VAS_Score,Mobility,Medical_History,
+                  EPT_current_pass,EPT_VAS_before_anaesthesia,periodontal_space,PDL_Ligament_involvement,
+                  Lamina_Dura,local_Anesthetic]]          
+          df = pd.DataFrame(data,columns=['Patient_Gender','Patient_Age','Pulp_Stone_or_Calcification','Pain_Duration_Days'
+                                          ,'Percussion_Test','Palpation','EPT_duration_before_anaesthesia_seconds'
+                                          ,'Dental_History','Curved_Canal','Cold_Test_Pain_Duration_before_anaesthesia_seconds'
+                                          ,'Cold_test_VAS_Score_Before_anaesthesia','Clinical_Pain_VAS_Score','Mobility'
+                                          ,'Medical_History','EPT_current_pass','EPT_VAS_before_anaesthesia','PDL_Space'
+                                          ,'PDL_Ligament_involvement','Lamina_Dura','local_Anesthetic'])
+          saved_final_rf = load_model('Final_ET_Model_12June2023')
+          new_prediction = predict_model(saved_final_rf, data=df)
+          df['prediction_label'] = new_prediction['prediction_label'][0]
+          df['prediction_score'] = new_prediction['prediction_score'][0]
+          submitted = st.form_submit_button("Submit Data for Prediction")
+
+  if submitted:
+        # saved_final_rf = load_model('Final_ET_Model_12June2023')
+        # new_prediction = predict_model(saved_final_rf, data=df)
+        #st.dataframe(new_prediction)
+        st.write("#### Necessity for Supplement (Prediction): ", df['prediction_label'][0])
+        st.write("#### Prediction Probability Score: ", df['prediction_score'][0])
+        #st.write(df)
+
+with st.container():
+  with st.form("my_form", clear_on_submit=True):
+          ground_truth = st.selectbox('Actual Operative or Ground Truth (Do this patient need supplement or not)',['','Yes-Supplement Anesthesia Needed','No-Supplement Anesthesia Not Needed'],format_func=lambda x: 'Select an option' if x == '' else x)
+          df['ground_truth'] = ground_truth
+          prediction_label = df['prediction_label'][0]
+          submitted = st.form_submit_button("Submit")
+  if submitted:
+    input_date = str(day) + "/" + str(month) + "/" +str(year)
+    year_month = str(year) + "_" + str(month)
+    period = str(year)
+    db.insert_data(str(datetime.utcnow()),input_date,period,Patient_Gender,Patient_Age,Pulp_Stone_or_Calcification,
+                          Pain_Duration_Days,Percussion_Test,Palpation,EPT_duration_before_anaesthesia_seconds,
+                          Dental_History,Curved_Canal,Cold_Test_Pain_Duration_before_anaesthesia_seconds,
+                          Cold_test_VAS_Score_Before_anaesthesia,Clinical_Pain_VAS_Score,Mobility,Medical_History,
+                          EPT_current_pass,EPT_VAS_before_anaesthesia,periodontal_space,PDL_Ligament_involvement,
+                          Lamina_Dura,local_Anesthetic,prediction_label,ground_truth)
+    #st.write(df)
+                  
+     
+
+
       
-      # st.dataframe(df)
-      saved_final_rf = load_model('Final_ET_Model_12June2023')
-      new_prediction = predict_model(saved_final_rf, data=df)
-      #st.dataframe(new_prediction)
-      st.write("#### Necessity for Supplement (Prediction): ", new_prediction['prediction_label'][0])
-      st.write("#### Prediction Probability Score: ", new_prediction['prediction_score'][0])
+
+      
+            
+      
+      
+      
+
       
 
       
--- database.py
+++ database.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 # Load the environment variables
 load_dotenv(".env")
-DETA_KEY = os.getenv("DETA_KEY")
-# DETA_KEY = st.secrets["DETA_KEY"]
+# DETA_KEY = os.getenv("DETA_KEY")
+DETA_KEY = st.secrets["DETA_KEY"]
 
 # Initialize with a project key
 deta = Deta(DETA_KEY)
@@ -15,12 +15,39 @@ deta = Deta(DETA_KEY)
 # This is how to create/connect a database
 db = deta.Base("suppliment_info")
 
-def insert_period(current, input_date, period, year_month, cat_income, incomes, cat_expenses, expenses, comment):
+def insert_data(current, input_date,period,Patient_Gender,Patient_Age,Pulp_Stone_or_Calcification,
+                 Pain_Duration_Days,Percussion_Test,Palpation,EPT_duration_before_anaesthesia_seconds,
+                 Dental_History,Curved_Canal,Cold_Test_Pain_Duration_before_anaesthesia_seconds,
+                 Cold_test_VAS_Score_Before_anaesthesia,Clinical_Pain_VAS_Score,Mobility,Medical_History,
+                 EPT_current_pass,EPT_VAS_before_anaesthesia,periodontal_space,PDL_Ligament_involvement,
+                 Lamina_Dura,local_Anesthetic,model_prediction,ground_truth):
     """Returns the report on a successful creation, otherwise raises an error"""
-    return db.put({"key": current, "input_date": input_date, "period": period, "year_month": year_month, "incomes_cat": cat_income,"incomes": incomes, "expenses_cat": cat_expenses,"expenses": expenses, "comment": comment})
+    return db.put({"key": current, "input_date": input_date, "period": period, 
+                   "Patient_Gender":Patient_Gender,"Patient_Age": Patient_Age,
+                   "Pulp_Stone_or_Calcification":Pulp_Stone_or_Calcification,
+                    "Pain_Duration_Days":Pain_Duration_Days,
+                    "Percussion_Test":Percussion_Test,
+                    "Palpation":Palpation,
+                    "EPT_duration_before_anaesthesia_seconds":EPT_duration_before_anaesthesia_seconds,
+                    "Dental_History":Dental_History,
+                    "Curved_Canal":Curved_Canal,
+                    "Cold_Test_Pain_Duration_before_anaesthesia_seconds":Cold_Test_Pain_Duration_before_anaesthesia_seconds,
+                    "Cold_test_VAS_Score_Before_anaesthesia":Cold_test_VAS_Score_Before_anaesthesia,
+                    "Clinical_Pain_VAS_Score":Clinical_Pain_VAS_Score,
+                    "Mobility":Mobility,
+                    "Medical_History":Medical_History,
+                    "EPT_current_pass":EPT_current_pass,
+                    "EPT_VAS_before_anaesthesia":EPT_VAS_before_anaesthesia,
+                    "periodontal_space":periodontal_space,
+                    "PDL_Ligament_involvement":PDL_Ligament_involvement,
+                    "Lamina_Dura":Lamina_Dura,
+                    "local_Anesthetic":local_Anesthetic,
+                    "new_prediction":model_prediction,
+                    "ground_truth":ground_truth                
+                   })
 
 
-def fetch_all_periods():
+def fetch_all_data():
     """Returns a dict of all periods"""
     res = db.fetch()
     return res.items
